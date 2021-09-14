7d8835c9c6ea606870c18fe05bd95dc1469a97cd
mschultz366 <mschultz366@students.mchenry.edu>
2021-09-14 16:07:53 -0500

    AboutMe program

---
+++ src/Aboutme.kt
@@ -0,0 +1,6 @@
+fun main(){
+    println("My name is McKenna.")
+    println("I am currently getting my Associate's in Science.")
+    println("I want to continue my education in Marine biology")
+    println("I enjoy fishing and being outdoors!")
+}
